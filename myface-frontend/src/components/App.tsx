89be5a962d535177285930d6d8fe2219bbbed460
Anastasia C <clarkanastasia@outlook.com>
2024-02-07 12:54:34 +0000

    add routing in App component

--- myface-frontend/src/components/App.tsx
+++ myface-frontend/src/components/App.tsx
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import '../css/App.css'
 import PostList from './PostList'
+import UserDetail from './UserDetail'
+import UserList from './UserList'
 
 export default function App() {
   return(
-    <PostList />
+      <Router>
+        <Routes>
+          <Route path= "/posts" element = {<PostList/>}/>
+          <Route path = "/users/" element = {<UserList/>}/> 
+          <Route path = "/users/:userId/" element = {<UserDetail/>}/> 
+          <Route path='*' element= {
+            <div>
+            <div> Oops, this page doesn't exits. Maybe you were looking for:</div>
+            <Link to="/posts"> Posts </Link>
+            <Link to="/users/">Users</Link>
+            </div>
+          }/>  
+        </Routes>
+      </Router>
   )
 }
