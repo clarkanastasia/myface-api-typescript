159e720156c3b8df0069117b84492ee87208204f
Anastasia C <clarkanastasia@outlook.com>
2024-02-08 17:40:31 +0000

    add form validation

--- myface-frontend/src/components/CreateUser.tsx
+++ myface-frontend/src/components/CreateUser.tsx
@@ -1,6 +1,16 @@
 import {CreateUserRequest} from '../models/api/createUserRequest'
 import { useState, FormEventHandler, ChangeEventHandler } from "react";
 
+export interface ErrorMessageProps{
+    isValidated : boolean;
+    errors : string | undefined;
+}
+
+function ErrorMessage(props : ErrorMessageProps){
+    if (props.isValidated === false){
+        return <div>Submit failed: {props.errors}</div>
+    }
+}
 
 export default function CreateUser(){
     const [user, setUser] = useState<CreateUserRequest>({
@@ -11,39 +21,61 @@ export default function CreateUser(){
     coverImageUrl:'',  
     })
 
+    const [isValidated, setIsValidated] = useState<boolean>(true);
+    const [errors, setErrors]= useState<string | undefined>(undefined);
+
+    const validateInput = (user : CreateUserRequest) => {
+        let error : string = ''; 
+
+    if(user.username.length < 2){
+        error = "the username is too short";
+        setErrors(error);
+        setIsValidated(false);
+    } 
+    if(user.email === ''){ 
+        error="email cannot be empty";
+        setErrors(error);
+        setIsValidated(false);
+    }
+}
+
 const handleChange: ChangeEventHandler<HTMLInputElement> = (event) =>{
     const {name, value} = event.target;
-    setUser(prev => ({...prev, [name]:value}))
-} 
+    setUser(prev => ({...prev, [name]:value}));
+}
 
-const handleSubmit:FormEventHandler<HTMLFormElement> = (e) => {
+const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     console.log(user);
+    validateInput(user);
 
-    fetch("http://localhost:3001/users/create", {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    })
-        .then((response) => {
-            if (response.status !== 200) {
-            throw new Error(response.statusText);
-            }
+    if(isValidated === true) {
+        fetch("http://localhost:3001/users/create", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
         })
-        .then(() => {
-            console.log("We'll be in touch soon.");
-        })
-    .catch((err) => {
-        console.log(err.toString());
-    });
+            .then((response) => {
+                if (response.status !== 200) {
+                throw new Error(response.statusText);
+                }
+            })
+            .then(() => {
+                console.log("Success! A new user has been created.");
+            })
+        .catch((err) => {
+            console.log(err.toString());
+        });
+    }
     };
 
     return(
         <div className="flexContainer">
             <h1>Create User</h1>
+            <ErrorMessage errors={errors} isValidated={isValidated} />
             <form onSubmit={handleSubmit}>
                 <div className="formContainer">
                 <label>
