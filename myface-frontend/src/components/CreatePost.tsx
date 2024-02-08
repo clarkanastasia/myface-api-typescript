c1f4b608df6ef241dcdb037d77148d9c09ae03ba
Anastasia C <clarkanastasia@outlook.com>
2024-02-08 18:24:59 +0000

    update component to handle form submit

--- myface-frontend/src/components/CreatePost.tsx
+++ myface-frontend/src/components/CreatePost.tsx
@@ -1,18 +1,58 @@
+import {CreatePostRequest} from '../models/api/createPostRequest'
+import { useState, FormEventHandler, ChangeEventHandler } from 'react'
 
 export default function CreatePost(){
+    
+    const [input, setInput] = useState<CreatePostRequest>({
+        message : '',
+        imageUrl : '',
+    })
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) =>{
+        const {name, value} = event.target;
+        setInput(prev => ({...prev, [name]:value}));
+    }
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(input);
+        fetch("http://localhost:3001/posts/create", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(input),
+        })
+            .then((response) => {
+                if (response.status !== 200) {
+                throw new Error(response.statusText);
+                }
+            })
+            .then(() => {
+                console.log("Success! A new post has been created.");
+            })
+        .catch((err) => {
+            console.log(err.toString());
+        });
+    }
 
     return(
-        <div>
+        <div className="flexContainer">
             <h1>Create Post</h1>
-            <form method="post" action="/posts/create">
+            <form onSubmit={handleSubmit}>
+                <div className="formContainer">
                 <label>
                     Message:
-                    <input type="text" name="message"/>
+                    <br/>
+                    <input type="text" name="message" value={input.message} onChange={handleChange}/>
                 </label>
                 <label>
                     ImageUrl:
-                    <input type="text" name="imageUrl"/>
+                    <br/>
+                    <input type="text" name="imageUrl" value={input.imageUrl} onChange={handleChange}/>
                 </label>
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
